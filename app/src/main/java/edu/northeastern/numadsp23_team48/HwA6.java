9d55613f7b3f6072d4da618d5c46bd28789518f7
Evangeline <yang.wenxin@northeastern.edu>
2023-02-13 18:51:26 -0800

    initial commit

---
+++ app/src/main/java/edu/northeastern/numadsp23_team48/HwA6.java
@@ -0,0 +1,14 @@
+package edu.northeastern.numadsp23_team48;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HwA6 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hw_a6);
+    }
+}
---
+++ app/src/main/java/edu/northeastern/numadsp23_team48/MainActivity.java
@@ -0,0 +1,33 @@
+package edu.northeastern.numadsp23_team48;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnA6 = findViewById(R.id.btn_a6);
+    Button btnA7 = findViewById(R.id.btn_a7);
+    Button btnGroup = findViewById(R.id.btn_group_project);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnA6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToA6Activity();
+            }
+        });
+
+    }
+
+    public void goToA6Activity(){
+        Intent intent = new Intent(this, HwA6.class);
+        startActivity(intent);
+    }
+}
