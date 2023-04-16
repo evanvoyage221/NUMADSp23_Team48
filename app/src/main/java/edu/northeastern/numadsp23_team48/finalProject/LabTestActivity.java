29faf1923cc84558d8b4e8773037ccfb96bfa06d
xmeng <75476123+Mmona-meng@users.noreply.github.com>
2023-04-16 15:42:49 -0700

    complete labtest layout, and modify UI design components

---
+++ app/src/main/java/edu/northeastern/numadsp23_team48/finalProject/LabTestActivity.java
@@ -0,0 +1,16 @@
+package edu.northeastern.numadsp23_team48.finalProject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.northeastern.numadsp23_team48.R;
+
+public class LabTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lab_test);
+    }
+}
